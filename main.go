3d64d03f5fc698d9ef0aa1ebe67cf269b527afa4
Maël Valais <mael.valais@cyberark.com>
2026-04-02 10:59:17 +0200

    fixup: login-bearer no longer exists

--- main.go
+++ main.go
@@ -72,7 +72,6 @@ func main() {
 		loginCmd("auth"),
 		loginWifCmd("auth"),
 		loginKeypairCmd("auth"),
-		loginBearerCmd("auth"),
 		loginTSGCmd("auth"),
 		switchCmd("auth"),
 		tenantidCmd("auth"),
